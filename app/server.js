72f80b1388ec95d9b5156ea05f63788bed0a0b5c
Bindushree C <bindusreec06@gmail.com>
2026-02-23 12:31:37 +0530

    Initial commit

---
+++ app/server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Simple Schema
+const TaskSchema = new mongoose.Schema({
+    title: String
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+// CREATE
+app.post("/tasks", async (req, res) => {
+    const task = await Task.create(req.body);
+    res.json(task);
+});
+
+// READ
+app.get("/tasks", async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+// DELETE
+app.delete("/tasks/:id", async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: "Deleted" });
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
